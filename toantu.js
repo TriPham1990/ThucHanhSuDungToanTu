5f3c2d55f2298f37921c5b024767a2ab5523bcaf
= <=>
2019-07-26 21:20:15 +0700

    thuchanh

---
+++ toantu.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width")
+inputHeight = prompt("Enter the height")
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Enter Width :" + '<br>' + inputWidth +'<br>');
+document.write("Enter Height :" + '<br>' + inputHeight +'<br>');
+document.write("The area is: " + area);
